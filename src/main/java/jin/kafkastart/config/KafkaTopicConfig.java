120c1ff631541ec08d4b27ebc177848e5522de90
Jinseonghyun <jinsunghyun9@gmail.com>
2024-08-28 15:02:56 +0900

    NewTopic 발행할 메서드 수정

--- src/main/java/jin/kafkastart/config/KafkaTopicConfig.java
+++ src/main/java/jin/kafkastart/config/KafkaTopicConfig.java
@@ -1,14 +1,15 @@
 package jin.kafkastart.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
-public class NewTopic {
+public class KafkaTopicConfig {
 
     @Bean
-    public org.apache.kafka.clients.admin.NewTopic newTopic() {
+    public NewTopic jinTopic() {
         return TopicBuilder
                 .name("jin")
                 .build();
