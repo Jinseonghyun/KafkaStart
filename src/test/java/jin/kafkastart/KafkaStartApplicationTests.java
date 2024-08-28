aa186d4e52e0ae1e66186e936628e7c022dc1bc0
Jinseonghyun <jinsunghyun9@gmail.com>
2024-08-28 13:44:54 +0900

    프로젝트 준비

---
+++ src/test/java/jin/kafkastart/KafkaStartApplicationTests.java
@@ -0,0 +1,13 @@
+package jin.kafkastart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
