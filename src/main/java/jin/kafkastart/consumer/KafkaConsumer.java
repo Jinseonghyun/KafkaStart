4cf2b5756a9d85b00374edf9a71c7b37d0e45084
Jinseonghyun <jinsunghyun9@gmail.com>
2024-08-28 15:23:21 +0900

    KafkaConsumer 생성

---
+++ src/main/java/jin/kafkastart/consumer/KafkaConsumer.java
@@ -0,0 +1,17 @@
+package jin.kafkastart.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import static java.lang.String.format;
+
+@Service
+@Slf4j
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "jin", groupId = "myGroup")
+    public void consumeMsg(String msg) {
+        log.info(format("Consuming the message from jin Topic: %s", msg));
+    }
+}
